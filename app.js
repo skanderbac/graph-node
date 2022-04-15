bb0345e1249b56123241f7142febfc631f150ff6
skanderbac <skanderbac@users.noreply.github.com>
2022-04-15 01:26:46 +0100

    add find meetings route

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors');
-const Router = require('./routes/Route');
-const bodyparser = require("body-parser");
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 const session = require('express-session');
 const flash = require('connect-flash');
 const msal = require('@azure/msal-node');
 require('dotenv').config();
 
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const authRouter = require('./routes/auth');
+const calendarRouter = require('./routes/calendar');
 
-var app = express();
+const app = express();
 
+// <MsalInitSnippet>
 // In-memory storage of logged-in users
 // For demo purposes only, production apps should store
 // this in a reliable storage
@@ -39,10 +44,14 @@ const msalConfig = {
 
 // Create msal application object
 app.locals.msalClient = new msal.ConfidentialClientApplication(msalConfig);
+// </MsalInitSnippet>
 
-
+// <SessionSnippet>
+// Session middleware
+// NOTE: Uses default in-memory session store, which is not
+// suitable for production
 app.use(session({
-  secret: '7d72f1d8-bd4a-4638-aab9-c5d403d12a89',
+  secret: process.env.OAUTH_CLIENT_SECRET,
   resave: false,
   saveUninitialized: false,
   unset: 'destroy'
@@ -72,24 +81,34 @@ app.use(function(req, res, next) {
 
   next();
 });
-app.use(cors());
-app.use(cors({origin: '*'}));
+// </SessionSnippet>
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+// <FormatDateSnippet>
+var hbs = require('hbs');
+var parseISO = require('date-fns/parseISO');
+var formatDate = require('date-fns/format');
+// Helper to format date/time sent by Graph
+hbs.registerHelper('eventDateTime', function(dateTime) {
+  const date = parseISO(dateTime);
+  return formatDate(date, 'M/d/yy h:mm a');
+});
+// </FormatDateSnippet>
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyparser());
-app.use(express.json({limit:'200mb'}));
-app.use(bodyparser.urlencoded({extended:true}));
-app.use('/uploadsFolder', express.static(path.join(__dirname, '/uploads')));
 
-app.use('/auth',Router);
+app.use('/', indexRouter);
+app.use('/auth', authRouter);
+app.use('/calendar', calendarRouter);
+app.use('/users', usersRouter);
 
-
-app.get("/",(req,res)=>{
-  res.sendFile(path.join(__dirname, '/views/index.html'));
-})
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -97,9 +116,6 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-
-
-   // Pass to next layer of middleware
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
